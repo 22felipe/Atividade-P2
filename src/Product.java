3f73e70f8c66735fad1204648bbc2b561a0dbe3b
Felipe Gabriel <felipegmarques32fg@gmail.com>
2025-07-29 22:43:35 -0300

    teste

---
+++ src/Product.java
@@ -0,0 +1,18 @@
+public class Product {
+    public String name;
+    public double price;
+    public int quantity;
+
+    public double TotalValueStock(){
+        return this.price *this.quantity;
+    }
+
+    public void AddProducts (int quantity){
+        this.quantity += quantity;
+    }
+
+    public void RemoveProducts(int quantity){
+        this.quantity -= quantity;
+    }
+
+}
