c638fb692c74b4a6e724d617e05fd4f2ba0a21a2
22felipe <116601150+22felipe@users.noreply.github.com>
2025-08-05 14:09:51 -0300

    Atualizar o Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,13 @@
 import java.util.Locale;
 import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
         Scanner sc= new Scanner(System.in);
 
-        int quantity;
-
+        
         Product n_product = new Product();
         System.out.println("Enter product data: ");
         System.out.print("Name: ");
